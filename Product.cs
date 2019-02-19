9266c73bf01dfafdba3cb1431bd3dd1ae5ffb6e5
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-02-19 16:19:45 +0100

    vending 2

--- Product.cs
+++ Product.cs
@@ -8,20 +8,23 @@ namespace VendingMachine
     {
         static int countId = 1;
 
-        public int Id { get; private set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
+        public int Id { get; private set; }//ger ett unikt id och sätter de privat så man ej kan se de utifrån
+        public string Name { get; set; }//Name som finns för alla produkter sätts här
+        public int Price { get; set; }//samma som namn
 
         public Product(string name, int price)
         {
-            Id = countId++;
-            Name = name;
-            Price = price;
+            Id = countId++;//plussar på idet med 1 per produkt
+            Name = name;// initierar variablen name
+            Price = price;//samma som ovan men med price
         }
-
+        
+       
         public override string ToString()
         {
-            return Id + ": " + Name;
+            return Id + ": " + Name;//returnerar id och name till en string
+       
         }
+        
     }
 }
--- chocolate.cs
+++ chocolate.cs
@@ -11,6 +11,7 @@ namespace VendingMashine
             this.color = color;
 
             this.size = size;
+          
 
         }
 
